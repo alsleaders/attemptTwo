f3c4908e203bf760cab9b9514e5a36e14c5614e3
Amanda Leaders <alsleaders@gmail.com>
2019-07-06 17:46:33 -0400

    added current drives

---
+++ ClientApp/src/pages/CurrentDrive.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class CurrentDrive extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Your Current Drive</h1>
+        </header>
+      </div>
+    )
+  }
+}
+
+export default CurrentDrive
