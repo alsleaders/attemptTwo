06436c685289c8407002405d73a596b89e575a24
Amanda Leaders <alsleaders@gmail.com>
2019-07-07 09:59:50 -0400

    page loads, missed table next

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,20 +1,12 @@
 import React, { useState, useEffect } from 'react'
+import './Home.css'
+
 import axios from 'axios'
-import ReactMapGL, {
-  Marker,
-  Popup,
-  NavigationControl
-  // MapboxDirections
-} from 'react-map-gl'
+import ReactMapGL, { Marker, Popup, NavigationControl } from 'react-map-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
-// import MapboxDirections from 'mapbox-gl-directions'
 
 const TOKEN =
   'pk.eyJ1IjoiYWxzbGVhZGVycyIsImEiOiJjang1aXNrcGkwMmR5M3lsZzg4OXFyNWRqIn0.qQib-cz84tOegHyTyc0U9g'
-// let directions = {}
-// let accessToken = ''
-// let unit = ''
-// let profile = ''
 
 export default function Home() {
   const [view, setView] = useState({
@@ -27,10 +19,7 @@ export default function Home() {
   const [selectedInfo, setSelectedInfo] = useState(null)
   const [mapData, setMapData] = useState([])
   const [currentLocation, setCurrentLocation] = useState('')
-  // const [currentCoord, setCurrentCoord] = useState([])
   const [plannedDestination, setPlannedDestination] = useState('')
-  // const [plannedCoord, setPlannedCoord] = useState([])
-  // var MapboxDirections = require('../src/index')
 
   useEffect(() => {
     axios.get('https://localhost:5001/api/location').then(resp => {
@@ -49,10 +38,6 @@ export default function Home() {
       .then(currentLocationResp => {
         console.log(currentLocationResp.data)
         console.log(currentLocationResp.data.features[0].center)
-        // setCurrentCoord({
-        //   longitude={currentLocation.data.features[0].center[0],
-        //     latitude={currentLocation.data.features[0].center[1]
-        //     });
         axios
           .get(
             `https://api.mapbox.com/geocoding/v5/mapbox.places/${plannedDestination}.json?access_token=${TOKEN}`
@@ -66,8 +51,6 @@ export default function Home() {
                 Long: currentLocationResp.data.features[0].center[0],
                 Lat: currentLocationResp.data.features[0].center[1],
                 Destination: [{ tripId: 2 }]
-                //     currentCoord
-                //     // {latitude={currentLocation.data.features[0].center[1]}, longitude={currentLocation.data.features[0].center[0]}
               })
               .then(resp => {
                 console.log(resp.data)
@@ -95,7 +78,7 @@ export default function Home() {
   }
 
   return (
-    <main>
+    <section>
       <section className="mapView">
         <ReactMapGL
           {...view}
@@ -109,14 +92,6 @@ export default function Home() {
         >
           <div style={{ position: 'absolute', left: 0 }}>
             <NavigationControl />
-            {/* <MapboxDirections /> */}
-            {/* <MapboxDirections
-              {...directions(
-                (accessToken = 'TOKEN'),
-                (unit = 'metric'),
-                (profile = 'mapbox/cycling')
-              )}
-            /> */}
           </div>
           {mapData.map(city => {
             return (
@@ -169,6 +144,63 @@ export default function Home() {
           </form>
         </div>
       </section>
-    </main>
+    </section>
   )
 }
+
+// export class Home extends Component {
+//   static displayName = Home.name
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>Hello, world!</h1>
+//         <p>Welcome to your new single-page application, built with:</p>
+//         <ul>
+//           <li>
+//             <a href="https://get.asp.net/">ASP.NET Core</a> and{' '}
+//             <a href="https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx">
+//               C#
+//             </a>{' '}
+//             for cross-platform server-side code
+//           </li>
+//           <li>
+//             <a href="https://facebook.github.io/react/">React</a> for
+//             client-side code
+//           </li>
+//           <li>
+//             <a href="http://getbootstrap.com/">Bootstrap</a> for layout and
+//             styling
+//           </li>
+//         </ul>
+//         <p>To help you get started, we have also set up:</p>
+//         <ul>
+//           <li>
+//             <strong>Client-side navigation</strong>. For example, click{' '}
+//             <em>Counter</em> then <em>Back</em> to return here.
+//           </li>
+//           <li>
+//             <strong>Development server integration</strong>. In development
+//             mode, the development server from <code>create-react-app</code> runs
+//             in the background automatically, so your client-side resources are
+//             dynamically built on demand and the page refreshes when you modify
+//             any file.
+//           </li>
+//           <li>
+//             <strong>Efficient production builds</strong>. In production mode,
+//             development-time features are disabled, and your{' '}
+//             <code>dotnet publish</code> configuration produces minified,
+//             efficiently bundled JavaScript files.
+//           </li>
+//         </ul>
+//         <p>
+//           The <code>ClientApp</code> subdirectory is a standard React
+//           application based on the <code>create-react-app</code> template. If
+//           you open a command prompt in that directory, you can run{' '}
+//           <code>npm</code> commands such as <code>npm test</code> or{' '}
+//           <code>npm install</code>.
+//         </p>
+//       </div>
+//     )
+//   }
+// }
