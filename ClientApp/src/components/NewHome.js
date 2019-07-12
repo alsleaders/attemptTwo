b7770d05b322176ab68e41540bd4c09b3b22f1c4
Amanda Leaders <alsleaders@gmail.com>
2019-07-12 09:43:00 -0400

    updated styling

--- ClientApp/src/components/NewHome.js
+++ ClientApp/src/components/NewHome.js
@@ -381,7 +381,7 @@ class NewHome extends Component {
         </section>
         <hr />
         <section className="input-fields">
-          <h3>Where did you come from? Where did you go?</h3>
+          {/* <h3>Where do we go now?</h3> */}
           <div className="input-column">
             <form onSubmit={this.planTrip}>
               <input
